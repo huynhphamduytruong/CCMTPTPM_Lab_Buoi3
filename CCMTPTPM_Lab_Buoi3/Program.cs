6b87cd55469148cac075746a104500252ea6e009
truongtran <xuantruong020899@gmail.com>
2021-03-31 13:55:13 +0700

    update

--- CCMTPTPM_Lab_Buoi3/Program.cs
+++ CCMTPTPM_Lab_Buoi3/Program.cs
@@ -11,6 +11,7 @@ namespace CCMTPTPM_Lab_Buoi3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Thegioi!");
 
             Console.ReadLine();
         }
