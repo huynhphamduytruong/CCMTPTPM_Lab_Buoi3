e8f9084030612dec306844363b7de692b0599892
Trường Huỳnh <25319326+huynhphamduytruong@users.noreply.github.com>
2021-03-31 13:53:02 +0700

    Commit test C# project

---
+++ CCMTPTPM_Lab_Buoi3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCMTPTPM_Lab_Buoi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+        }
+    }
+}
